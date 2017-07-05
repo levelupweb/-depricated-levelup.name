6b9410c5a0dbaa30b0774b2ccadd120c854bc316
kv9991 <exide-pra@yandex.ru>
2017-07-06 01:42:48 +0300

    Deleting no req dependencies

---
+++ utils/initScripts.js
@@ -0,0 +1,8 @@
+export default () => {
+	window.$ = require('jquery');
+   require('../static/css/ui/semantic.min.js');
+}
+
+export function UI() {
+	require('../static/css/ui/semantic.min.js');
+}
